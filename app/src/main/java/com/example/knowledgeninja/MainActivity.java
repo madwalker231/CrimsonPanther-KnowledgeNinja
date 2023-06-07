fac682c91914f4e8d1ea139ad6fc4589b98b3b20
Graham Murray <GLMurray@student.fullsail.edu>
2023-06-07 12:31:29 -0500

    This is a very early version of what I'm working on for the information page. Colors/fonts/images can all be changed. Just thought this would be a good place to start. Also added a button to the home page to access the fact page.

--- app/src/main/java/com/example/knowledgeninja/MainActivity.java
+++ app/src/main/java/com/example/knowledgeninja/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.knowledgeninja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button informationButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button informationButton = findViewById(R.id.buttonInformationPage);
+        informationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, InformationActivity.class));
+            }
+        });
     }
 }
