dd2ee31a8f0c8c6668565b0f55ec91591bb1dd72
Madison Walker <madwalker231@gmail.com>
2023-06-20 23:46:51 -0700

    Main Menu work 20Jun23. Added appbar to main menu and added images and text to space suptopic menu.

--- app/src/main/java/com/example/knowledgeninja/Menu.java
+++
@@ -1,7 +0,0 @@
-package com.example.knowledgeninja;
-
-public class Menu {
-    View popupTriggerView = findViewById(R.id.your_trigger_view);
-
-    popupTriggerView.setOnClickListener(v -> showPopupMenu(v));
-}
