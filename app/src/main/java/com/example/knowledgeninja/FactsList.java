e62d84d6c7041cce80a44394c58db819e1e281e8
Graham Murray <GLMurray@student.fullsail.edu>
2023-06-27 20:57:44 -0500

    6/27/2023 Rename

--- app/src/main/java/com/example/knowledgeninja/FactsList.java
+++ app/src/main/java/com/example/knowledgeninja/FactsList.java
@@ -24,7 +24,7 @@ public class FactsList  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_facts_list);
 
         editTextSearch = findViewById(R.id.editTextSearch);
         imageViewSearch = findViewById(R.id.imageViewSearch);
